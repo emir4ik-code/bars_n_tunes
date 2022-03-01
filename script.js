a77ac83dc8ab6857098338624adac926971406c8
emir4ik-code <emirahmetov@gmail.com>
2022-03-01 16:01:12 -0500

    artist name and song name appended to DOM  on search

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('search-button');
     const searchVal = document.getElementById('search-val');
+    const artistName = document.getElementById('artist-name');
+    const songName = document.getElementById('song-name');
+    const lyrics = document.getElementById('lyrics');
      searchButton.addEventListener('click', ()=> {
         fetch(`https://genius-song-lyrics1.p.rapidapi.com/search?q=${searchVal.value}`, {
             "method": "GET",
@@ -11,7 +14,10 @@ window.addEventListener('DOMContentLoaded', () => {
         })
         .then(res => res.json())
         .then(data => {
-            console.log(data.response.hits[0].result.full_title) //
+            artistName.appendChild(document.createTextNode(data.response.hits[0].result.artist_names))
+            songName.appendChild(document.createTextNode(data.response.hits[0].result.title))
+            console.log(data.response.hits[0].result)
+            //
         })
      })
 })
