b685971e1b9c246f5a85b6423472d5a4cd6288ed
emir4ik-code <emirahmetov@gmail.com>
2022-03-03 08:51:15 -0500

    .

---
+++ youtube_fetch.js
@@ -0,0 +1,17 @@
+// const fetchYoutube = () => {
+//     fetch('https://www.googleapis.com/youtube/v3/search?part=snippet')
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+// }
+
+// fetchYoutube();
+
+// function searchByKeyword() {
+//     var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
+//     for(var i in results.items) {
+//       var item = results.items[i];
+//       Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
+//     }
+//   }
+
+//   searchByKeyword();
