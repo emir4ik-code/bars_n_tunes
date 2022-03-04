2e04c0c0bab63944599537f83526e7f080c6fa17
Gurupriyan <gurupriyan98@gmail.com>
2022-03-04 08:53:16 -0500

    Final changes and error handling

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("DOM content loaded is not working")
 window.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('search-button');
     const searchVal = document.getElementById('search-val');
@@ -6,16 +5,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const songName = document.getElementById('song-name');
     const lyrics = document.getElementById('lyrics');
     const albumPic = document.getElementById('albumPic');
-    const showVidButton = document.getElementById('showVideo')
     let meaning = document.getElementById('meaning')
     let partOfSpeech = document.getElementById('partOfSpeech')
     let example = document.getElementById('example')
     let theWord = document.getElementById('word')
     searchButton.addEventListener('click', ()=> {
-        //showVidButton.addEventListener('click', () => 
-        //searchVideo(searchVal.value)
-        //)
-        console.log("Im here")
+
+        searchVideo(searchVal.value);
+        
         let songURL = `https://genius.p.rapidapi.com`
     fetch(`${songURL}/search?q=${searchVal.value}`, {
         "method": "GET",
@@ -29,11 +26,9 @@ window.addEventListener('DOMContentLoaded', () => {
             artistName.innerText = data.response.hits[0].result.artist_names;
             songName.innerText = data.response.hits[0].result.title;
             albumPic.src = data.response.hits[0].result.header_image_thumbnail_url;
-            console.log(data) //.response.hits[0].result
             let songID = data.response.hits[0].result.id;
             
-            // let showLyrics = document.getElementById("showLyrics");
-            //showLyrics.addEventListener('click', () => {
+            //put button here for lyrics if needed 
                 
                 fetch(`https://genius-song-lyrics1.p.rapidapi.com/songs/${songID}/lyrics`, {
                     "method": "GET",
@@ -44,84 +39,73 @@ window.addEventListener('DOMContentLoaded', () => {
                 })
                 .then(res => res.json())
                 .then(data => {
-                    //console.log(data);
-                    lyrics.innerHTML = data.response.lyrics.lyrics.body.html;
+                    if (data.meta.status === 200) {
+                        lyrics.innerHTML = data.response.lyrics.lyrics.body.html;
 
-                    // lyrics.innerHTML = data.response.lyrics.lyrics.body.html;
-                    var string = document.getElementsByTagName('p')[0].innerHTML;
-                    console.log(string)
-                    var strippedHtml = string.replace(/<(?!br\s*\/?)[^>]+>/g, '')
-                    console.log(strippedHtml)
-                    let lastSTR = strippedHtml.split("<br>").join("3")
-                    //console.log(lastSTR)
-                    let lastSTR2 = lastSTR.split("33").join("3")
-                    //console.log(lastSTR2)
-                    let lastSTR3 = lastSTR2.split("3").join(" 3 ")
-                    //console.log(lastSTR3)
+                        var string = document.getElementsByTagName('p')[0].innerHTML;
+                        var strippedHtml = string.replace(/<(?!br\s*\/?)[^>]+>/g, '')
+                        let lastSTR = strippedHtml.split("<br>").join("3");
 
-                    let punctuationless = lastSTR3.replace(/[.,\/#!$%\^?&\*;:{}=\-_`~]+/g,"");
-                    var finalString = punctuationless.replace(/\s{2,}/g," ");
-                    //let finalString2 = finalString.replace(/[\[\]']+/g,'')
-                    console.log(finalString)
-                    ////let bigArr = qu.split(' 3 ')
-                    //lyrics.innerHTML = finalString2
-
-
-                    let text3 = finalString.split(" "); 
-                    console.log(text3)
-                    let result3 = "";
-                    text3.forEach(function(char){
-                        // Append a new span only if the current char is not a space
-                        //if(char !== '3'){
-                            result3 += (char.trim() === "") ? "" : "<span class=\"text\">" + char + "</span> ";
-                        //}
-                    }); 
-
-                    console.log(result3)
-
-                    // let h1 = document.getElementById("h1ex")
-                    //h1.innerHTML = `${finalString2}`
-                    let text4 = result3.split("<span class=\"text\">3</span>"); 
-                    console.log(text4)
-                    let result4 = "";
-                    text4.forEach(function(char){
-                        // Append a new span only if the current char is not a space
-                        result4 += (char.trim() === "") ? "" : "<p>" + char + "</p><br>";
-                    }); 
-                    console.log(result4)
-                    lyrics.innerHTML = result4;
-
-                    let elementsList = document.getElementsByClassName("text");
-                    console.log(elementsList)
-
-                    for (let item of elementsList) {
-                        item.addEventListener("click", function() {
-                            //this function does stuff
-                            console.log(item.innerText)
-                            let word = item.innerText
-                            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-                            .then(res => res.json())
-                            .then(data => {
-                                const capitalizeFirstLetter = ([ first, ...rest ], locale = navigator.language) =>
-                                first.toLocaleUpperCase(locale) + rest.join('')
-                                theWord.innerText = capitalizeFirstLetter(`${data[0].word}`)
-                                partOfSpeech.innerText = data[0].meanings[0].partOfSpeech
-                                meaning.innerText = data[0].meanings[0].definitions[0].definition
-                                if(data[0].meanings[0].definitions[0].example !== undefined){
-                                    example.innerText = `Example: ${data[0].meanings[0].definitions[0].example}`
-                                } else {
-                                    example.innerText = "Example: Unavailable"
-                                }
-                                console.log(data[0].meanings[0].definitions[0].definition)
-                                
-                              
-                            })
-                            .catch(err => console.log(err))
-                        });
-                    };
+                        let lastSTR2 = lastSTR.split("33").join("3");
+                        
+                        let lastSTR3 = lastSTR2.split("3").join(" 3 ");
                     
+
+                        let punctuationless = lastSTR3.replace(/[.,\/#!$%\^?&\*;:{}=\-_`~]+/g,"");
+                        var finalString = punctuationless.replace(/\s{2,}/g," ");
+                        //let finalString2 = finalString.replace(/[\[\]']+/g,'')
+                        ////let bigArr = qu.split(' 3 ')
+
+                        let text3 = finalString.split(" "); 
+                        let result3 = "";
+                        text3.forEach(function(char){
+                            // Append a new span only if the current char is not a space
+                            //if(char !== '3'){
+                                result3 += (char.trim() === "") ? "" : "<span class=\"text\">" + char + "</span> ";
+                            //}
+                        }); 
+
+                        // let h1 = document.getElementById("h1ex")
+                        //h1.innerHTML = `${finalString2}`
+                        let text4 = result3.split("<span class=\"text\">3</span>"); 
+                        let result4 = "";
+                        text4.forEach(function(char){
+                            // Append a new span only if the current char is not a space
+                            result4 += (char.trim() === "") ? "" : "<p>" + char + "</p><br>";
+                        }); 
+                        //final adding of lyrics
+                        lyrics.innerHTML = result4;
+
+                        let elementsList = document.getElementsByClassName("text");
+
+                        for (let item of elementsList) {
+                            item.addEventListener("click", function() {
+                                //this function does stuff
+                                console.log(item.innerText)
+                                let word = item.innerText
+                                fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+                                .then(res => res.json())
+                                .then(data => {
+                                    const capitalizeFirstLetter = ([ first, ...rest ], locale = navigator.language) =>
+                                    first.toLocaleUpperCase(locale) + rest.join('')
+                                    theWord.innerText = capitalizeFirstLetter(`${data[0].word}`)
+                                    partOfSpeech.innerText = data[0].meanings[0].partOfSpeech
+                                    meaning.innerText = data[0].meanings[0].definitions[0].definition
+                                    if(data[0].meanings[0].definitions[0].example !== undefined){
+                                        example.innerText = `Example: ${data[0].meanings[0].definitions[0].example}`
+                                    } else {
+                                        example.innerText = "Example: Unavailable"
+                                    }
+                                })
+                                .catch(err => console.log(err))
+                            });
+                        };
+                    } else if (data.meta.status === 404) {
+                        lyrics.innerHTML = `
+                        <h6>Coudn't find lyrics for this song.</h6>
+                        `
+                    }
                 }); 
-           // })
         })
      })
 })
