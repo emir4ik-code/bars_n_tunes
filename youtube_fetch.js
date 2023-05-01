65326cab68d015665f43a86309ffb6816100ca71
Emir Akhmetov <91210955+emir4ik-code@users.noreply.github.com>
2023-05-01 19:44:46 -0400

    Update youtube_fetch.js

--- youtube_fetch.js
+++ youtube_fetch.js
@@ -1,7 +1,7 @@
 const videoDiv = document.getElementById('videoDiv')
 
 async function searchVideo(queryString) {
-await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${queryString}&key=AIzaSyBsc6n71jOPosg0OQf_Nk4M2HKeb2C8a_E` )
+await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${queryString}&key=AIzaSyDLqepHGYhn2MH84wCLS4ueckRONGKjSxg` )
 .then(response => response.json())
 .then(data => {
 const videoLink = `https://www.youtube.com/embed/${data.items[0].id.videoId}`
